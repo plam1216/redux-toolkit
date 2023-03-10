30f0aa31ca6ad5f3f20646b20ccd666f4046c1b3
Peter Lam <plam1216@gmail.com>
2023-03-10 14:00:20 -0500

    redux calculator

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice'
+
+
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.count)
+    const dispatch = useDispatch()
+
+    const [incrementAmount, setIncrementAmount] = useState(0)
+
+    const addValue = Number(incrementAmount) || 0
+
+    const resetAll = () => {
+        setIncrementAmount(0)
+        dispatch(reset())
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <div>
+                <button onClick={() => dispatch(increment())}>+</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+
+            <input
+                type="text"
+                value={incrementAmount}
+                onChange={(e) => setIncrementAmount(e.target.value)}
+            />
+
+            <div>
+                <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+                <button onClick={resetAll}>Reset</button>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
